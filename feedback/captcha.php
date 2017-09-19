2dc2d57976a9a0ad26b4db9d5cef4422efb90802
Pavel <palasin@yahoo.com>
2017-09-19 15:28:39 +0300

    feedback php fix

--- feedback/captcha.php
+++ feedback/captcha.php
@@ -9,18 +9,18 @@ $captchastring = substr(str_shuffle($captchastring), 0, 6);
 // содержащей 6 символов
 $_SESSION["code"] = $captchastring;
 // генерируем CAPTCHA
-// создаем новое изображение из файла background.png 
-$image = imagecreatefrompng(dirname(__FILE__).'/background.png');
+// создаем новое изображение из файла background.png
+$image = imagecreatefrompng(dirname(__FILE__).'../feedback/background.png');
 // устанавливаем цвет (R-200, G-240, B-240) изображению, хранящемуся в $image
 $colour = imagecolorallocate($image, 200, 240, 240);
 // присваиваем переменной font название шрифта
-$font = dirname(__FILE__).'/oswald.ttf';
-// устанавливаем случайное число между -10 и 10 градусов для поворота текста 
+$font = dirname(__FILE__).'../feedback/oswald.ttf';
+// устанавливаем случайное число между -10 и 10 градусов для поворота текста
 $rotate = rand(-10, 10);
-// рисуем текст на изображении шрифтом TrueType (1 параметр - изображение ($image), 
-// 2 - размер шрифта (18), 3 - угол поворота текста ($rotate), 
+// рисуем текст на изображении шрифтом TrueType (1 параметр - изображение ($image),
+// 2 - размер шрифта (18), 3 - угол поворота текста ($rotate),
 // 4, 5 - начальные координаты x и y для текста (18,30), 6 - индекс цвета ($colour),
-// 7 - путь к файлу шрифта ($font), 8 - текст ($captchastring) 
+// 7 - путь к файлу шрифта ($font), 8 - текст ($captchastring)
 imagettftext($image, 18, $rotate, 28, 32, $colour, $font, $captchastring);
 // передавать изображение будем в формате png
 header('Content-type: image/png');
